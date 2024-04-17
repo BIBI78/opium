1ec4fcbd31c2c7ef396d6a0f11ced42500f33de5
BIBI78 <blakecollins78@gmail.com>
2024-04-17 14:05:06 +0000

    UPDATE: just formatted Beat.js and evryting still works , was worried since there is so much code

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,22 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
-
-import Form from "react-bootstrap/Form";
-import Alert from "react-bootstrap/Alert";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Image from "react-bootstrap/Image";
-import Container from "react-bootstrap/Container";
-
 import { Link, useHistory } from "react-router-dom";
-
+import { Form, Alert, Button, Col, Row, Image, Container } from "react-bootstrap";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
-
 import signinImage from "../../assets/signin.jpg";
 
 function SignInForm() {
@@ -30,8 +20,8 @@ function SignInForm() {
   const { username, password } = signInData;
 
   const [errors, setErrors] = useState({});
-
   const history = useHistory();
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -105,7 +95,7 @@ function SignInForm() {
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
-            Don't have an account? Create one here:  <span>Sign up</span>
+            Don't have an account? Create one here: <span>Sign up</span>
           </Link>
         </Container>
       </Col>
@@ -113,10 +103,7 @@ function SignInForm() {
         md={6}
         className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
       >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={signinImage}
-        />
+        <Image className={`${appStyles.FillerImage}`} src={signinImage} />
       </Col>
     </Row>
   );
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-
-import styles from "../../styles/SignInUpForm.module.css";
-import btnStyles from "../../styles/Button.module.css";
-import appStyles from "../../App.module.css";
-
-import signupImage from "../../assets/signup.jpg";
-
 import {
   Form,
   Button,
@@ -18,6 +11,10 @@ import {
 } from "react-bootstrap";
 import axios from "axios";
 import { useRedirect } from "../../hooks/useRedirect";
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import signupImage from "../../assets/signup.jpg";
 
 const SignUpForm = () => {
   useRedirect("loggedIn");
@@ -29,7 +26,6 @@ const SignUpForm = () => {
   const { username, password1, password2 } = signUpData;
 
   const [errors, setErrors] = useState({});
-
   const history = useHistory();
 
   const handleChange = (event) => {
@@ -131,11 +127,7 @@ const SignUpForm = () => {
         md={6}
         className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
       >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          // src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"}
-           src={signupImage}
-        />
+        <Image className={`${appStyles.FillerImage}`} src={signupImage} />
       </Col>
     </Row>
   );
